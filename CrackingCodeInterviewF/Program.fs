bd02837bd02a714aa0aeafdc2bee9f83f203b9e5
Michal <mkowalik@outlook.com>
2015-10-04 22:28:51 +0200

    -- added fizbuzz game

--- CrackingCodeInterviewF/Program.fs
+++ CrackingCodeInterviewF/Program.fs
@@ -11,8 +11,26 @@ let sieve max =
         | n :: tail -> n :: next (List.filter(fun x -> x % n <> 0) tail)
     next [2..max]
 
+// fizzbuzz game:
+let fizzy num =
+    match num % 3, num % 5 with
+        | 0, 0 -> "fizzbuzz"
+        | 0, _ -> "fizz"
+        | _, 0 -> "buzz"
+        | _, _ -> num.ToString()
+
+let fizzbuzz max = 
+    [1..max]
+    |> List.map fizzy
+    |> List.iter (fun (s: string) -> printfn "%s" s)
+
+
 [<EntryPoint>]
 let main argv = 
+    // primes:
     printfn "%A" (sieve 1000)
-    0 // return an integer exit code
-    
+
+    //fizzbuzz:
+    fizzbuzz 100
+
+    0 // return an integer exit code    
