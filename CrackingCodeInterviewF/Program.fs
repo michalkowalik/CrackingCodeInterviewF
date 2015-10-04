5630b2cf5fb49157b424496920ab8bcff42ffd25
Michal <mkowalik@outlook.com>
2015-10-04 21:03:31 +0200

    sieve of eratosthenes - initial commit.

---
+++ CrackingCodeInterviewF/Program.fs
@@ -0,0 +1,18 @@
+﻿#light
+
+open System
+open System.Collections.Generic
+
+// sieve of Eratosthenes
+let sieve max =
+    let rec next p = 
+        match p with
+        | [] -> []
+        | n :: tail -> n :: next (List.filter(fun x -> x % n <> 0) tail)
+    next [2..max]
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" (sieve 1000)
+    0 // return an integer exit code
+    
